a43d6ffad071bec400a19b5cf4aeabfb05a072db
Roodly Jean Paul <188526128+RoodlyJP@users.noreply.github.com>
2026-02-04 03:34:11 +0000

    feat: create add, substract, multiply and divid function

---
+++ script.js
@@ -0,0 +1,24 @@
+// Add function
+function add(number1, number2) {
+    return number1 + number2
+}
+//Substract function
+function substract(number1, number2) {
+    return number1 - number2
+}
+//Multiply function
+function multiply(number1, number2) {
+    return number1 * number2
+}
+//Divid function
+function divid(number1, number2) {
+    return number1 / number2
+}
+
+let a = 3
+let b = 5
+
+console.log(multiply(a, b))
+console.log(add(a, b))
+console.log(substract(a, b))
+console.log(divid(a, b))
