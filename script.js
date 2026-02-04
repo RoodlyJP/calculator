412c22c272d143b8d8e9534f9fed4577fb9558aa
Roodly Jean Paul <188526128+RoodlyJP@users.noreply.github.com>
2026-02-04 04:06:39 +0000

    feat:create function operate and add clear button

--- script.js
+++ script.js
@@ -15,10 +15,21 @@ function divid(number1, number2) {
     return number1 / number2
 }
 
-let a = 3
-let b = 5
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = null;
 
-console.log(multiply(a, b))
-console.log(add(a, b))
-console.log(substract(a, b))
-console.log(divid(a, b))
+function operate(num1, num2, oper) {
+    switch(oper){
+        case '+': 
+            return add(num1, num2);
+        case '-':
+            return substract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divid(num1, num2);
+        default: 
+            return "ERROR!";
+    }
+}
