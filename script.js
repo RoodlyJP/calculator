b4d171e5bb1ab6c158bf0e05b006810e7001d2fd
Roodly Jean Paul <188526128+RoodlyJP@users.noreply.github.com>
2026-04-11 00:28:27 +0000

    I am back

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
 // Add function
 function add(number1, number2) {
-    return number1 + number2
+    return number1 + number2;
 }
-//Substract function
+// Substract function
 function substract(number1, number2) {
-    return number1 - number2
+    return number1 - number2;
 }
-//Multiply function
+// Multiply function
 function multiply(number1, number2) {
-    return number1 * number2
+    return number1 * number2;
 }
-//Divid function
+// Divid function
 function divid(number1, number2) {
-    return number1 / number2
+    return number1 / number2;
 }
 
-let firstNumber = 0;
-let secondNumber = 0;
-let operator = null;
-
-function operate(num1, num2, oper) {
-    switch(oper){
-        case '+': 
-            return add(num1, num2);
-        case '-':
-            return substract(num1, num2);
-        case '*':
-            return multiply(num1, num2);
-        case '/':
-            return divid(num1, num2);
-        default: 
-            return "ERROR!";
-    }
-}
